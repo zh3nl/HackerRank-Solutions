323c7711c7fd81acb5a5522dba2da51947e0e9d0
Zhen Liu <127721103+zh3nl@users.noreply.github.com>
2024-08-10 23:25:46 -0700

    Create main.py

---
+++ Birthday Cake Candles/main.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'birthdayCakeCandles' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY candles as parameter.
+#
+
+def birthdayCakeCandles(candles):
+    # Write your code here
+    max = 0
+    counter = 0
+    for i in candles:
+        if i == max:
+            counter += 1
+        elif i > max:
+            max = i
+            counter = 1
+    return counter
+            
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    candles_count = int(input().strip())
+
+    candles = list(map(int, input().rstrip().split()))
+
+    result = birthdayCakeCandles(candles)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
