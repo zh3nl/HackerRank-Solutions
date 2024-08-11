86481626851c453b8bdbf84adf4f19b6cd182b6f
Zhen Liu <127721103+zh3nl@users.noreply.github.com>
2024-08-10 23:24:46 -0700

    Create main.py

---
+++ Staircase/main.py
@@ -0,0 +1,23 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'staircase' function below.
+#
+# The function accepts INTEGER n as parameter.
+#
+
+def staircase(n):
+    # Write your code here
+    for x in range(1, n + 1):
+        print(f'{"#"*x:>{n}}')
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    staircase(n)
